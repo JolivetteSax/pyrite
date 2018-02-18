6b75b185fc645a570a7dfd8182adf685a1bd2e1c
JolivetteSax <erikjsax@gmail.com>
2018-02-18 16:24:11 -0500

    added some comments to explain better

--- ledger/spend.py
+++ ledger/spend.py
@@ -1,6 +1,10 @@
 import json
 
+# unspent is where we'll store unspent transactions. 
 unspent={}
+
+# This is the new list of transactions that spend the outputs loaded above
+# Each transaction *must* spend the entirety of the previous output
 transactions={}
 
 with open("unspent.json", 'r') as file_in:
@@ -9,12 +13,14 @@ with open("unspent.json", 'r') as file_in:
 with open("transactions.json", 'r') as file_in:
   transactions = json.load(file_in)
 
+# Some book-keeping - we need to number the transactions by finding the highest numbered one
 max_id=0
 for item in unspent:
   num=int(item[6:])
   if num > max_id:
     max_id = num
 
+# Going through the transactions, we're matching them up with unspent outputs
 for tx in transactions:
   print "Processing:" + tx['id']
   if unspent.has_key(tx['id']):
